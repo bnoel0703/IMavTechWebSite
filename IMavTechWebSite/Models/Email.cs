35d34c2298ada8d46779cc3a8aaccd76b9040dbd
Bryan Noel <bnoel0703@gmail.com>
2019-02-24 18:40:55 -0500

    Added email class

---
+++ IMavTechWebSite/Models/Email.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IMavTechWebSite.Models
+{
+    public class Email
+    {
+        public string SenderName { get; set; }
+        public string SenderEmailAddress { get; set; }
+        public string EmailSubject { get; set; }
+        public string EmailDescription { get; set; }
+    }
+}
